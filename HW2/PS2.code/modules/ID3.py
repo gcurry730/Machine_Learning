07315990dcb784d932d0af8c8177eb40f7cf30d0
gcurry730 <gcurry730@gmail.com>
2016-05-02 22:38:17 -0500

    Finished pick_best_attribute()

--- HW2/PS2.code/modules/ID3.py
+++ HW2/PS2.code/modules/ID3.py
@@ -45,63 +45,7 @@ def check_homogenous(data_set):
 #print check_homogenous(data_set) ==  None
 #data_set = [[1],[1],[1],[1],[1],[1]]
 #print check_homogenous(data_set) ==  1
-
-
-def pick_best_attribute(data_set, attribute_metadata, numerical_splits_count):
-    '''
-    ========================================================================================================
-    Input:  A data_set, attribute_metadata, splits counts for numeric
-    ========================================================================================================
-    Job:    Find the attribute that maximizes the gain ratio. If attribute is numeric return best split value.
-            If nominal, then split value is False.
-            If gain ratio of all the attributes is 0, then return False, False
-            Only consider numeric splits for which numerical_splits_count is greater than zero
-    ========================================================================================================
-    Output: best attribute, split value if numeric
-    ========================================================================================================
-    '''
-    # Your code here
-    pass
-
-# # ======== Test Cases =============================
-# numerical_splits_count = [20,20]
-# attribute_metadata = [{'name': "winner",'is_nominal': True},{'name': "opprundifferential",'is_nominal': False}]
-# data_set = [[1, 0.27], [0, 0.42], [0, 0.86], [0, 0.68], [0, 0.04], [1, 0.01], [1, 0.33], [1, 0.42], [0, 0.51], [1, 0.4]]
-# pick_best_attribute(data_set, attribute_metadata, numerical_splits_count) == (1, 0.51)
-# attribute_metadata = [{'name': "winner",'is_nominal': True},{'name': "weather",'is_nominal': True}]
-# data_set = [[0, 0], [1, 0], [0, 2], [0, 2], [0, 3], [1, 1], [0, 4], [0, 2], [1, 2], [1, 5]]
-# pick_best_attribute(data_set, attribute_metadata, numerical_splits_count) == (1, False)
-
-# Uses gain_ratio_nominal or gain_ratio_numeric to calculate gain ratio.
-
-def mode(data_set):
-    '''
-    ========================================================================================================
-    Input:  A data_set
-    ========================================================================================================
-    Job:    Takes a data_set and finds mode of index 0.
-    ========================================================================================================
-    Output: mode of index 0.
-    ========================================================================================================
-    '''
-    count_1 = 0
-    count_0 = 0    
-    for index in range(len(data_set)):
-        if data_set[index][0] is 1:
-            count_1 = count_1 + 1
-        if data_set[index][0] is 0:
-            count_0 = count_0 + 1
-    if count_1 >= count_0:
-        return 1
-    else:
-        return 0 
-    
-# ======== Test case =============================
-#data_set = [[0],[1],[1],[1],[1],[1]]
-#print mode(data_set) == 1
-#data_set = [[0],[1],[0],[0]]
-#print mode(data_set) == 0
-
+        
 def entropy(data_set):
     '''
     ========================================================================================================
@@ -136,33 +80,8 @@ def entropy(data_set):
 #data_set = [[0],[0],[1],[1],[0],[1],[1],[0]]
 #print entropy(data_set) == 1.0
 #data_set = [[0],[0],[0],[0],[0],[0],[0],[0]]
-#print entropy(data_set) == 0
-
-def split_on_nominal(data_set, attribute):
-    '''
-    ========================================================================================================
-    Input:  subset of data set, the index for a nominal attribute.
-    ========================================================================================================
-    Job:    Creates a dictionary of all values of the attribute.
-    ========================================================================================================
-    Output: Dictionary of all values pointing to a list of all the data with that attribute
-    ========================================================================================================
-    '''
-    
-    dictionary= {}    
-    for index in range (len(data_set)): 
-         key = data_set[index][attribute]   
-         dictionary.setdefault(key,[])            
-         dictionary[key].append(data_set[index])
-             
-    return dictionary
-    
-# ======== Test case =============================
-#data_set, attr = [[0, 4], [1, 3], [1, 2], [0, 0], [0, 0], [0, 4], [1, 4], [0, 2], [1, 2], [0, 1]], 1
-#print split_on_nominal(data_set, attr) == {0: [[0, 0], [0, 0]], 1: [[0, 1]], 2: [[1, 2], [0, 2], [1, 2]], 3: [[1, 3]], 4: [[0, 4], [0, 4], [1, 4]]}
-#data_set, attr = [[1, 2], [1, 0], [0, 0], [1, 3], [0, 2], [0, 3], [0, 4], [0, 4], [1, 2], [0, 1]], 1
-#print split_on_nominal(data_set, attr) == {0: [[1, 0], [0, 0]], 1: [[0, 1]], 2: [[1, 2], [0, 2], [1, 2]], 3: [[1, 3], [0, 3]], 4: [[0, 4], [0, 4]]}
-
+#print entropy(data_set) == 0        
+        
 def gain_ratio_nominal(data_set, attribute):
     '''
     ========================================================================================================
@@ -210,7 +129,7 @@ def gain_ratio_nominal(data_set, attribute):
 #print gain_ratio_nominal(data_set,attr)== 0.2056423328155741
 #data_set, attr = [[0, 3], [0, 3], [0, 3], [0, 4], [0, 4], [0, 4], [0, 0], [0, 2], [1, 4], [0, 4]], 1
 #print gain_ratio_nominal(data_set,attr)== 0.06409559743967516
-
+    
 def split_on_numerical(data_set, attribute, splitting_value):
     '''
     ========================================================================================================
@@ -236,7 +155,8 @@ def split_on_numerical(data_set, attribute, splitting_value):
 #print split_on_numerical(d_set,a,sval) == ([[1, 0.25], [1, 0.19], [1, 0.34], [1, 0.19]],[[1, 0.89], [0, 0.93], [0, 0.48], [1, 0.49], [0, 0.6], [0, 0.6]])
 #d_set,a,sval = [[0, 0.91], [0, 0.84], [1, 0.82], [1, 0.07], [0, 0.82],[0, 0.59], [0, 0.87], [0, 0.17], [1, 0.05], [1, 0.76]],1,0.17
 #print split_on_numerical(d_set,a,sval) == ([[1, 0.07], [1, 0.05]],[[0, 0.91],[0, 0.84], [1, 0.82], [0, 0.82], [0, 0.59], [0, 0.87], [0, 0.17], [1, 0.76]])
-
+    
+    
 def gain_ratio_numeric(data_set, attribute, steps):
     '''
     ========================================================================================================
@@ -282,15 +202,116 @@ def gain_ratio_numeric(data_set, attribute, steps):
         
         
 # ======== Test case =============================
-data_set,attr,step = [[0,0.05], [1,0.17], [1,0.64], [0,0.38], [0,0.19], [1,0.68], [1,0.69], [1,0.17], [1,0.4], [0,0.53]], 1, 2
-gain_ratio_numeric(data_set,attr,step) 
-#== (0.21744375685031775, 0.64)
-data_set,attr,step = [[1, 0.35], [1, 0.24], [0, 0.67], [0, 0.36], [1, 0.94], [1, 0.4], [1, 0.15], [0, 0.1], [1, 0.61], [1, 0.17]], 1, 4
-gain_ratio_numeric(data_set,attr,step) 
-#== (0.11689800358692547, 0.94)
-data_set,attr,step = [[1, 0.1], [0, 0.29], [1, 0.03], [0, 0.47], [1, 0.25], [1, 0.12], [1, 0.67], [1, 0.73], [1, 0.85], [1, 0.25]], 1, 1
-gain_ratio_numeric(data_set,attr,step) 
-#== (0.23645279766002802, 0.29)
+#data_set,attr,step = [[0,0.05], [1,0.17], [1,0.64], [0,0.38], [0,0.19], [1,0.68], [1,0.69], [1,0.17], [1,0.4], [0,0.53]], 1, 2
+#print gain_ratio_numeric(data_set,attr,step) == (0.21744375685031775, 0.64)
+#data_set,attr,step = [[1, 0.35], [1, 0.24], [0, 0.67], [0, 0.36], [1, 0.94], [1, 0.4], [1, 0.15], [0, 0.1], [1, 0.61], [1, 0.17]], 1, 4
+#print gain_ratio_numeric(data_set,attr,step) == (0.11689800358692547, 0.94)
+#data_set,attr,step = [[1, 0.1], [0, 0.29], [1, 0.03], [0, 0.47], [1, 0.25], [1, 0.12], [1, 0.67], [1, 0.73], [1, 0.85], [1, 0.25]], 1, 1
+#print gain_ratio_numeric(data_set,attr,step) == (0.23645279766002802, 0.29)
+
+def pick_best_attribute(data_set, attribute_metadata, numerical_splits_count):
+    '''
+    ========================================================================================================
+    Input:  A data_set, attribute_metadata, splits counts for numeric
+    ========================================================================================================
+    Job:    Find the attribute that maximizes the gain ratio. If attribute is numeric return best split value.
+            If nominal, then split value is False.
+            If gain ratio of all the attributes is 0, then return False, False
+            Only consider numeric splits for which numerical_splits_count is greater than zero
+    ========================================================================================================
+    Output: best attribute, split value if numeric
+    ========================================================================================================
+    '''
+    best= 0
+    best_attr = 0
+    threshold = None
+    # gives # of attributes to choose from
+    for i in range(1, len(data_set[0])):
+        if attribute_metadata[i]['is_nominal']:           
+            if gain_ratio_nominal(data_set, i) > best:
+                 best = gain_ratio_nominal(data_set, i)
+                 best_attr = i 
+                 threshold = False
+        else:
+            if gain_ratio_numeric(data_set, i, 1)[0]> best:
+                best= gain_ratio_numeric(data_set, i, 1)[0]
+                best_attr = i 
+                threshold = gain_ratio_numeric(data_set, i, 1)[1]
+    return (best_attr, threshold)
+    
+
+# # ======== Test Cases =============================
+#numerical_splits_count = [20,20]
+#attribute_metadata = [{'name': "winner",'is_nominal': True},{'name': "opprundifferential",'is_nominal': False}]
+#data_set = [[1, 0.27], [0, 0.42], [0, 0.86], [0, 0.68], [0, 0.04], [1, 0.01], [1, 0.33], [1, 0.42], [0, 0.51], [1, 0.4]]
+#print pick_best_attribute(data_set, attribute_metadata, numerical_splits_count) == (1, 0.51)
+#
+#attribute_metadata = [{'name': "winner",'is_nominal': True},{'name': "weather",'is_nominal': True}]
+#data_set = [[0, 0], [1, 0], [0, 2], [0, 2], [0, 3], [1, 1], [0, 4], [0, 2], [1, 2], [1, 5]]
+#print pick_best_attribute(data_set, attribute_metadata, numerical_splits_count) == (1, False)
+#
+#data_set = [[0, 0, 0.1], [1, 0, 0.2], [0, 2, 0.2], [0, 2, 0.2], [0, 3, 0.1], [1, 1, 0.1], [0, 4, 0.1], [0, 2, 0.1], [1, 2, 0.1], [1, 5, 0.1]] 
+#attribute_metadata = [{'name': "winner",'is_nominal': True},{'name': "weather",'is_nominal': True}, {'name': "attitude", 'is_nominal': False}]
+#numerical_splits_count = [20,20,20,20]
+#print pick_best_attribute(data_set, attribute_metadata, numerical_splits_count) == (1,False)
+
+# Uses gain_ratio_nominal or gain_ratio_numeric to calculate gain ratio.
+
+def mode(data_set):
+    '''
+    ========================================================================================================
+    Input:  A data_set
+    ========================================================================================================
+    Job:    Takes a data_set and finds mode of index 0.
+    ========================================================================================================
+    Output: mode of index 0.
+    ========================================================================================================
+    '''
+    count_1 = 0
+    count_0 = 0    
+    for index in range(len(data_set)):
+        if data_set[index][0] is 1:
+            count_1 = count_1 + 1
+        if data_set[index][0] is 0:
+            count_0 = count_0 + 1
+    if count_1 >= count_0:
+        return 1
+    else:
+        return 0 
+    
+# ======== Test case =============================
+#data_set = [[0],[1],[1],[1],[1],[1]]
+#print mode(data_set) == 1
+#data_set = [[0],[1],[0],[0]]
+#print mode(data_set) == 0
+
+
+
+def split_on_nominal(data_set, attribute):
+    '''
+    ========================================================================================================
+    Input:  subset of data set, the index for a nominal attribute.
+    ========================================================================================================
+    Job:    Creates a dictionary of all values of the attribute.
+    ========================================================================================================
+    Output: Dictionary of all values pointing to a list of all the data with that attribute
+    ========================================================================================================
+    '''
+    
+    dictionary= {}    
+    for index in range (len(data_set)): 
+         key = data_set[index][attribute]   
+         dictionary.setdefault(key,[])            
+         dictionary[key].append(data_set[index])
+             
+    return dictionary
+    
+# ======== Test case =============================
+#data_set, attr = [[0, 4], [1, 3], [1, 2], [0, 0], [0, 0], [0, 4], [1, 4], [0, 2], [1, 2], [0, 1]], 1
+#print split_on_nominal(data_set, attr) == {0: [[0, 0], [0, 0]], 1: [[0, 1]], 2: [[1, 2], [0, 2], [1, 2]], 3: [[1, 3]], 4: [[0, 4], [0, 4], [1, 4]]}
+#data_set, attr = [[1, 2], [1, 0], [0, 0], [1, 3], [0, 2], [0, 3], [0, 4], [0, 4], [1, 2], [0, 1]], 1
+#print split_on_nominal(data_set, attr) == {0: [[1, 0], [0, 0]], 1: [[0, 1]], 2: [[1, 2], [0, 2], [1, 2]], 3: [[1, 3], [0, 3]], 4: [[0, 4], [0, 4]]}
+
 
 
 
